e47d9e8a062f0ab64e71ae1dfe9c363992bb00c8
Dinamo0809 <107805584+Dinamo0908@users.noreply.github.com>
2023-02-05 18:13:26 -0300

    Add files via upload

---
+++ Calculadora CSharp.cs
@@ -0,0 +1,37 @@
+//calculadora 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora {
+
+
+     class sum {
+         private int x;
+         private int y;
+         public Sum() {
+         Console.Write("Por favor, elige dos numeros para ser sumados:")
+         int x = Console.ReadLine(": ")
+         Console.Write("Hazlo nuevamente:")
+         int y = Console.ReadLine(": ")
+         public int S {
+             get {return x + y}
+             Console.WriteLine("El resultado es " + )
+            }
+         }
+     }
+    static void Main(string[] args) {
+        Console.Write("Bienvenido a esta calculadora. Que operacion quieres realizar?");
+        Console.WriteLine("Sumar (+), restar (-), multiplicar (*), dividir (/) ");
+        string ch1 = Console.ReadLine()
+        if ch1 == "+":
+         sum();
+        else if ch1 == "-":
+        else:
+         Console.Write("No te he entendido. Quieres hacerlo nuevamente?");
+     
+    }
+
+}
